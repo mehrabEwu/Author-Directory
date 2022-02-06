afe5a4de43ca538f24f78e44c58d90c6c3abebac
unknown <mehrab.sh1995@gmail.com>
2022-02-06 10:32:17 +0600

    final project

--- docs/main.js
+++ docs/main.js
@@ -27,7 +27,7 @@ __webpack_require__.r(__webpack_exports__);
 // The list of file replacements can be found in `angular.json`.
 const environment = {
     production: false,
-    apiUrl: "http://api.quotable.io"
+    apiUrl: "https://api.quotable.io"
 };
 /*
  * For easier debugging in development mode, you can import the following file
